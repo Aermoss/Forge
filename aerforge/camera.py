eb1d40a74f373656889368f919c5d2484478b145
Yusuf Rençber <86446300+Aermoss@users.noreply.github.com>
2021-10-16 03:01:10 +0300

    Delete camera.py

--- aerforge/camera.py
+++
@@ -1,8 +0,0 @@
-class Camera:
-    def __init__(self, window):
-        self.window = window
-
-    def set_pos(self, x, y):
-        for i in self.window.objects:
-            i.x = i.x - (x - self.window.width / 2)
-            i.y = i.y - (y - self.window.height / 2)
